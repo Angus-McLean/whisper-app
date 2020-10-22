87c8dddfb8c875bb7c1f6288707c55415a47c5b4
amclean <amclean@gorillagroup.com>
2020-10-22 01:38:09 -0400

    First commit - setup react app + basic chat structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import InputBox from './components/InputBox';
+import MessagesList from './components/MessagesList';
 
 function App() {
+
+  var state = {
+    messages: [
+      {
+        key:1,
+        type:'received',
+        text:'Pssst... Whisper is so amazing.. right?',
+        datetime:'12:00 PM | Aug 13'
+      },
+      {
+        key:2,
+        type:'sent',
+        text:'Zomg! It totally is! Wowowow',
+        datetime:'12:00 PM | Aug 13'
+      }
+    ]
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row rounded-lg overflow-hidden shadow">
+        <div className="col-5 px-0">
+          <div className="bg-white">
+            <div className="bg-gray px-4 py-2 bg-light">
+              <p className="h5 mb-0 py-1">Team  &nbsp;&nbsp; | &nbsp;&nbsp;&nbsp; Everyone</p>
+            </div>
+            <MessagesList messages={state.messages}/>
+            <InputBox messages={state.messages}/>
+          </div>
+        </div>
+        {/* <!-- Chat Box--> */}
+        <div className="col-7 px-0 bg-white">
+          <div className="px-4 py-5 text-center chat-box ">MAIN VIDEO WINDOW</div>
+        </div>
+      </div>
     </div>
   );
 }
