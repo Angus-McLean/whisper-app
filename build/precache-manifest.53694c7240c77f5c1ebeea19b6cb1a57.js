15c6609fff23f866caff0125b676753544995f76
amclean <amclean@gorillagroup.com>
2020-11-07 14:37:12 -0500

    adds build folder

---
+++ build/precache-manifest.53694c7240c77f5c1ebeea19b6cb1a57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c953c122546d62577d2b9cb56f4283c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adb1e686ac0d61a0b3c0",
+    "url": "/static/css/main.544c12b7.chunk.css"
+  },
+  {
+    "revision": "8c0f27fb603e8174d2de",
+    "url": "/static/js/2.2b1124dc.chunk.js"
+  },
+  {
+    "revision": "2d54400cfeffd5a03c97473febb60b1e",
+    "url": "/static/js/2.2b1124dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adb1e686ac0d61a0b3c0",
+    "url": "/static/js/main.6431fdff.chunk.js"
+  },
+  {
+    "revision": "3d54858f8fe71828c69b",
+    "url": "/static/js/runtime-main.3968212c.js"
+  }
+]);
