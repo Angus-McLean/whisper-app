17f71b0134db87205555e2d5ac3791a95f7a9549
amclean <amclean@gorillagroup.com>
2020-11-15 15:47:14 -0500

    build

---
+++ build/precache-manifest.56c1bd31796390b38f409046af2b824c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1cb1444ee18a2a5b10f61df0efa09d1",
+    "url": "/whisper-app/index.html"
+  },
+  {
+    "revision": "74d38e522a28de65123a",
+    "url": "/whisper-app/static/css/main.a57a2596.chunk.css"
+  },
+  {
+    "revision": "63cdb68a4e3525ceacea",
+    "url": "/whisper-app/static/js/2.afffbcc2.chunk.js"
+  },
+  {
+    "revision": "2d54400cfeffd5a03c97473febb60b1e",
+    "url": "/whisper-app/static/js/2.afffbcc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74d38e522a28de65123a",
+    "url": "/whisper-app/static/js/main.588c58d4.chunk.js"
+  },
+  {
+    "revision": "e33cc5228ce65c6385ce",
+    "url": "/whisper-app/static/js/runtime-main.ea8f29ee.js"
+  }
+]);
